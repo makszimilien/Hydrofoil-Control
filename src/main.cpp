d78e5984cb30f2f4880467c3a25223849b415aa8
makszimilien <nagy.tamas.kvk@gmail.com>
2023-08-18 23:04:34 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include <PID_v1.h>
+#include <Servo.h>
+
+Servo elevator1;
+// Min 50, max 130, mid 90
+int servoInput = 0;
+int servoPin = 2;
+
+// Min 390, max 430
+int sensorValue = 0;
+int sensorPin = 28;
+int sensorAVG = 0;
+
+int pwmPin = 3;
+unsigned long pwmValue = 0;
+
+void setup() {
+  pinMode(sensorPin, INPUT);
+  pinMode(pwmPin, INPUT);
+
+  Serial1.begin(115200);
+  Serial1.println("Serial started");
+
+  elevator1.attach(servoPin);
+  elevator1.write(90);
+}
+
+void loop() {
+  // if (Serial1.available() > 0) {
+  //   servoInput = Serial1.parseInt();
+  // }
+  // Serial1.println(servoInput);
+  // elevator1.write(servoInput);
+
+  // sensorValue = (analogRead(sensorPin) - 390) / ((430 - 390) / 20);
+  // sensorAVG = (sensorAVG + sensorValue) / 2;
+  // Serial1.println(sensorAVG);
+  // delay(500);
+
+  pwmValue = pulseIn(pwmPin, HIGH);
+  Serial1.println(pwmValue);
+}
