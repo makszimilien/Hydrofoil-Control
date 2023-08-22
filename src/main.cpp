8f50c61870335ccd75e9e19e35947c40ae2b4c32
makszimilien <nagy.tamas.kvk@gmail.com>
2023-08-22 18:30:41 +0200

    Add pulseIn timeout to reduce cycle time, fix setpoint value definition

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,16 @@ int factor = 20; // %, sets the effects of the PWM input on the PID setpoint
 
 double setpoint, input, output;
 // target is a setpoint value, sets the nominal sinking of the vehicle
-double target = 50;
+double target = 128; // Controls the sinking to the point where the water level
+                     // is at the middle of the sensor
 int kp = 2;
 int ki = 5;
 int kd = 1;
 PID elevator1Pid(&input, &output, &setpoint, kp, ki, kd, DIRECT);
+
+unsigned long prevTime = 0;
+unsigned long currTime = 0;
+int counter = 0;
 
 void setup() {
   pinMode(sensorPin, INPUT);
@@ -35,20 +40,20 @@ void setup() {
   elevator1.attach(servoPin);
   elevator1.write(90);
 
-  setpoint = 128; // Controls the sinking to the point where the water level is
-                  // at the middle of the sensor
   elevator1Pid.SetMode(AUTOMATIC);
 }
 
 void loop() {
   sensorValue = analogRead(sensorPin);
   sensorAVG = (sensorAVG + sensorValue) / 2;
-  pwmRead = pulseIn(pwmPin, HIGH);
-  if (pwmRead >= 990 && pwmRead <= 2010) {
-    pwmValue = map(pwmRead, 990, 2010, 0, 255);
-    control = (pwmValue - 128) / 100.0 * factor;
-  } else
-    control = 0;
+
+  // To modify setpoint value based on PWM input
+  // pwmRead = pulseIn(pwmPin, HIGH, 40000);
+  // if (pwmRead >= 990 && pwmRead <= 2010) {
+  //   pwmValue = map(pwmRead, 990, 2010, 0, 255);
+  //   control = (pwmValue - 128) / 100.0 * factor;
+  // } else
+  //   control = 0;
 
   input = map(sensorAVG, 0, 1023, 0, 255);
   setpoint = target + control;
@@ -57,11 +62,16 @@ void loop() {
   // Serial1.println(pwmRead);
   // Serial1.println(pwmValue - 128);
   // Serial1.println(control);
-  Serial1.println(setpoint);
-  Serial1.println(input);
-  Serial1.println(output);
-  Serial1.println("*******");
-  delay(500); // For serial pringing only
+  // Serial1.println(setpoint);
+  // Serial1.println(input);
+  // Serial1.println(output);
+  // Serial1.println("*******");
+  // delay(500); // For serial printing only
+
+  // For measuring cycle time
+  currTime = millis();
+  Serial1.println(currTime - prevTime);
+  prevTime = currTime;
 
   elevator1.write(map(output, 0, 255, 50, 130));
 }
