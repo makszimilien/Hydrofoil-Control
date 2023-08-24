6f1035610438ae007c2fee219eeaf508c70d2127
makszimilien <nagy.tamas.kvk@gmail.com>
2023-08-24 22:58:55 +0200

    Create new branch for Pro Micro

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <Arduino.h>
+#include <HX711.h>
 #include <PID_v1.h>
 #include <Servo.h>
 
 Servo elevator1;
 // Min 50, max 130, mid 90
-int servoInput = 0;
-int servoPin = 2;
+int servoPin = 4;
 
 int sensorValue = 0;
-int sensorPin = 28;
+int sensorPin = 18;
 int sensorAVG = 0;
 
-int pwmPin = 3;
+int pwmPin = 5;
 unsigned long pwmRead = 0;
 int pwmValue = 0;
 float control = 0;
@@ -26,6 +26,10 @@ int ki = 5;
 int kd = 1;
 PID elevator1Pid(&input, &output, &setpoint, kp, ki, kd, DIRECT);
 
+const int waterLvl1Dout = 2;
+const int waterLvl1Sck = 3;
+HX711 waterLvl1;
+
 unsigned long prevTime = 0;
 unsigned long currTime = 0;
 int counter = 0;
@@ -34,13 +38,15 @@ void setup() {
   pinMode(sensorPin, INPUT);
   pinMode(pwmPin, INPUT);
 
-  Serial1.begin(115200);
-  Serial1.println("Serial started");
+  Serial.begin(115200);
+  Serial.println("Serial started");
 
   elevator1.attach(servoPin);
   elevator1.write(90);
 
   elevator1Pid.SetMode(AUTOMATIC);
+
+  waterLvl1.begin(waterLvl1Dout, waterLvl1Sck);
 }
 
 void loop() {
@@ -58,20 +64,39 @@ void loop() {
   input = map(sensorAVG, 0, 1023, 0, 255);
   setpoint = target + control;
   elevator1Pid.Compute();
+  elevator1Pid.Compute();
+  elevator1Pid.Compute();
 
-  // Serial1.println(pwmRead);
-  // Serial1.println(pwmValue - 128);
-  // Serial1.println(control);
-  // Serial1.println(setpoint);
-  // Serial1.println(input);
-  // Serial1.println(output);
-  // Serial1.println("*******");
+  // Serial.println(pwmRead);
+  // Serial.println(pwmValue - 128);
+  // Serial.println(control);
+  // Serial.println(setpoint);
+  // Serial.println(input);
+  // Serial.println(output);
+  // Serial.println("*******");
   // delay(500); // For serial printing only
 
   // For measuring cycle time
   currTime = millis();
-  Serial1.println(currTime - prevTime);
+  Serial.println(currTime - prevTime);
   prevTime = currTime;
 
   elevator1.write(map(output, 0, 255, 50, 130));
+  elevator1.write(map(output, 0, 255, 50, 130));
+  elevator1.write(map(output, 0, 255, 50, 130));
+
+  // if (waterLvl1.wait_ready_timeout(1000)) {
+  //   long reading = waterLvl1.read();
+  //   Serial.print("HX711 reading: ");
+  //   Serial.println(reading);
+  // } else {
+  //   Serial.println("HX711 not found.");
+  // }
+  long reading = waterLvl1.read();
+  Serial.println(reading);
+  reading = waterLvl1.read();
+  Serial.println(reading);
+  reading = waterLvl1.read();
+  Serial.println(reading);
+  // delay(500);
 }
