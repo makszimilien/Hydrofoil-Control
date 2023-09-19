1d77ab4c99c94281f5f25cd156ea769f6b6247e1
makszimilien <nagy.tamas.kvk@gmail.com>
2023-09-19 21:05:32 +0200

    Fix websocket issue

--- data/wifimanager.js
+++ data/wifimanager.js
@@ -38,8 +38,5 @@ function onMessage(event) {
   const data = JSON.parse(event.data);
   const keys = Object.keys(data);
 
-  for (let i = 0; i < keys.length; i++) {
-    let key = keys[i];
-    document.getElementById(key).innerText = String(data[key]);
-  }
+  document.getElementById("mac").innerText = String(data["mac"]);
 }
