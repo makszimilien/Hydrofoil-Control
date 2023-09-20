6d45179d5f7f4647f87831eba460904fdfde7d7b
makszimilien <nagy.tamas.kvk@gmail.com>
2023-09-20 20:16:45 +0200

    Add slider, store value in config.json, set output accordingly, send value on ws

--- data/script.js
+++ data/script.js
@@ -1,5 +1,22 @@
 "use strict";
 
+// Get elements
+const slider = document.getElementById("slider");
+const sliderValue = document.getElementById("sliderValue");
+
+// Callback for slider
+const updateSlider = function (slider) {
+  const xhr = new XMLHttpRequest();
+  const value = slider.value;
+  const output = slider.id;
+  const params = new URLSearchParams();
+  params.append("value", value);
+  params.append("output", output);
+  xhr.open("POST", "/set-output", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send(params);
+};
+
 const gateway = `ws://${window.location.hostname}/ws`;
 let websocket;
 // Init web socket when the page loads
@@ -36,7 +53,18 @@ function onClose(event) {
 function onMessage(event) {
   console.log(event.data);
   const data = JSON.parse(event.data);
+
   const keys = Object.keys(data);
 
-  document.getElementById("slave").innerText = String(data["slave"]);
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    document.getElementById(key).innerHTML = data[key];
+    document.getElementById(`${key}Value`).innerHTML = data[key];
+    console.log();
+  }
 }
+
+// Update the value display when the slider value changes
+slider.addEventListener("change", function () {
+  updateSlider(this);
+});
