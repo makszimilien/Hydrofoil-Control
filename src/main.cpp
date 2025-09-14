19b5c9658bced017e8698ce7af92cbe1ad997159
makszimilien <nagy.tamas.kvk@gmail.com>
2025-09-14 20:39:45 +0200

    Store min and maxMeasured values separately for slaves

--- src/main.cpp
+++ src/main.cpp
@@ -151,14 +151,6 @@ void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 // stored in slave1 struct regardless of the actual slave identifier
 void onDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   memcpy(&boardsParams.slave1, incomingData, sizeof(boardsParams.slave1));
-  // Serial.print("RecievedMin: ");
-  // Serial.print(boardsParams.slave1.minMeasured);
-  // Serial.print(" RecievedMax: ");
-  // Serial.print(boardsParams.slave1.maxMeasured);
-  // Serial.print(" ControlMin: ");
-  // Serial.print(controlParams.minMeasured);
-  // Serial.print(" ControlMax: ");
-  // Serial.println(controlParams.maxMeasured);
   if (boardsParams.slave1.servoMin != controlParams.servoMin) {
     elevator.writeMicroseconds(boardsParams.slave1.servoMin);
     delayWhile(2000);
@@ -434,39 +426,39 @@ void calculatePid() {
 };
 
 // Log params to UART
-void logPid() {
-  // Serial.print("measured:");
-  // Serial.print(median);
-  // Serial.print(":");
-  // Serial.print("Input: ");
-  // Serial.print(input);
-  // Serial.print("  ");
-  // Serial.print("setpoint: ");
-  // Serial.print(setpoint);
-  // Serial.print("  Output: ");
-  // Serial.print(output);
-  // Serial.print("  ");
-  // Serial.print("PWM read: ");
-  // Serial.print(pwmRead);
-  // Serial.print("  PWM value: ");
-  // Serial.print(pwmRead);
-  // Serial.print("  Free heap memory: ");
-  // Serial.println(ESP.getFreeHeap());
-  // Serial.print(":");
-  // Serial.print("control:");
-  // Serial.println(control);
-  // Serial.print("kp:");
-  // Serial.print(controlParams.p);
-  // Serial.print(":");
-  // Serial.print("ki:");
-  // Serial.print(controlParams.i);
-  // Serial.print(":");
-  // Serial.print("kd:");
-  // Serial.println(controlParams.d);
-  Serial.print("MinMeasured: ");
-  Serial.print(controlParams.minMeasured);
-  Serial.print(" MaxMeasured: ");
-  Serial.println(controlParams.maxMeasured);
+void logPid(){
+    // Serial.print("measured:");
+    // Serial.print(median);
+    // Serial.print(":");
+    // Serial.print("Input: ");
+    // Serial.print(input);
+    // Serial.print("  ");
+    // Serial.print("setpoint: ");
+    // Serial.print(setpoint);
+    // Serial.print("  Output: ");
+    // Serial.print(output);
+    // Serial.print("  ");
+    // Serial.print("PWM read: ");
+    // Serial.print(pwmRead);
+    // Serial.print("  PWM value: ");
+    // Serial.print(pwmRead);
+    // Serial.print("  Free heap memory: ");
+    // Serial.println(ESP.getFreeHeap());
+    // Serial.print(":");
+    // Serial.print("control:");
+    // Serial.println(control);
+    // Serial.print("kp:");
+    // Serial.print(controlParams.p);
+    // Serial.print(":");
+    // Serial.print("ki:");
+    // Serial.print(controlParams.i);
+    // Serial.print(":");
+    // Serial.print("kd:");
+    // Serial.println(controlParams.d);
+    // Serial.print("MinMeasured: ");
+    // Serial.print(controlParams.minMeasured);
+    // Serial.print(" MaxMeasured: ");
+    // Serial.println(controlParams.maxMeasured);
 };
 
 // Set up tickers
@@ -667,8 +659,6 @@ void setupWifiMaster() {
           request->getParam("slider-servo-min", true)->value().toInt();
       tempParams.servoMax =
           request->getParam("slider-servo-max", true)->value().toInt();
-      tempParams.minMeasured = controlParams.minMeasured;
-      tempParams.maxMeasured = controlParams.maxMeasured;
 
       // Send success response
       request->send(200, "text/plain", "OK");
@@ -678,6 +668,8 @@ void setupWifiMaster() {
     }
     // Store received values, send slave params to devices
     if (boardSelector == "master") {
+      tempParams.minMeasured = controlParams.minMeasured;
+      tempParams.maxMeasured = controlParams.maxMeasured;
       boardsParams.master = tempParams;
       if (boardsParams.master.servoMin != controlParams.servoMin) {
         elevator.writeMicroseconds(boardsParams.master.servoMin);
@@ -701,6 +693,8 @@ void setupWifiMaster() {
     }
 
     else if (boardSelector == "slave-1") {
+      tempParams.minMeasured = boardsParams.slave1.minMeasured;
+      tempParams.maxMeasured = boardsParams.slave1.maxMeasured;
       boardsParams.slave1 = tempParams;
       if (strcmp(macAddresses[0].c_str(), "") != 0) {
         stringToMac(macAddresses[0], broadcastAddress);
@@ -710,6 +704,8 @@ void setupWifiMaster() {
     }
 
     else if (boardSelector == "slave-2") {
+      tempParams.minMeasured = boardsParams.slave2.minMeasured;
+      tempParams.maxMeasured = boardsParams.slave2.maxMeasured;
       boardsParams.slave2 = tempParams;
       if (strcmp(macAddresses[1].c_str(), "") != 0) {
         stringToMac(macAddresses[1], broadcastAddress);
